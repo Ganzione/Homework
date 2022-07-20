9f4d6f3384a411db1628291558bd2c284e34fade
Ganzione <109446715+Ganzione@users.noreply.github.com>
2022-07-20 11:06:02 +0900

    0720
    0720 homework

--- Homework0719/Homework0719/Program.cs
+++ Homework0719/Homework0719/Program.cs
@@ -10,7 +10,15 @@ namespace Homework0719
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello World");
+            string s1, s2;
+            System.Console.Write("input s1 : ");
+            s1 = System.Console.ReadLine();
+            System.Console.Write("input s2 : ");
+            s2 = System.Console.ReadLine();
+            System.Console.WriteLine("the string is" + "\n" + "{0}" + ", " + "{1}", s1, s2);
+            System.Console.WriteLine("the string is" + "\n" + s1 + ", " + s2);
+            System.Console.WriteLine($"the string is" + "\n" + "{0}" + ", " + "{1}", s1, s2);
+            System.Console.WriteLine($"the string is" + "\n" + s1 + ", " + s2);
             System.Console.ReadKey();
         }
     }
